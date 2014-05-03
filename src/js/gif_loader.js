90f1dfd2160f625da09372be26d892abcf17afb5
Jean Bordat <bordat.jean@gmail.com>
2014-05-04 00:36:41 +0200

    Update for main loader.
    Signed-off-by: Jean Bordat <bordat.jean@gmail.com>

--- src/js/gif_loader.js
+++ src/js/gif_loader.js
@@ -17,56 +17,81 @@ function gifLoader (el, options) {
         classOveride : false,
         borderRadius: 5,
         zIndex: 999
-    }
+    };
+    var buffer;
     this.options = jQuery.extend(defaults, options);
     this.container = $(el);
 	
 	/**
 	 * Init function
 	 */
-    this.init = function() {
+    this.init = function(buff) {
+
+        this.buffer = buff;
+
         var container = this.container;
 
-        // Delete previous active loaders in this element
-        this.remove(); 
+        if (this.buffer == 0) {
+            // Delete previous active loaders in this element
+            this.remove(); 
 
-        // Div "Loader" initialisation
-        var overlay = $('<div id="loader"></div>').css({
-            'background-color': this.options.bgColor,
-            'opacity':this.options.opacity,
-            'width': (container.width() + 0),
-            'height': (container.height() + 0),
-            'position':'relative',
-            'top':'0px',
-            'left':'0px',
-            'z-index':this.options.zIndex,
-            'margin-top': (1 - (container.height() + 1)),
-            'border-radius': this.options.borderRadius
-        }).addClass('gif_overlay');
+            // Div "Loader" initialisation
+            var overlay = $('<div id="loader"></div>').css({
+                'background-color': this.options.bgColor,
+                'opacity':this.options.opacity,
+                'width': (container.width() + 0),
+                'height': (container.height() + 0),
+                'position':'relative',
+                'top':'0px',
+                'left':'0px',
+                'z-index':this.options.zIndex,
+                'margin-top': (1 - (container.height() + 1)),
+                'border-radius': this.options.borderRadius
+            }).addClass('gif_overlay');
 
-        // Target class override in order to host the Loader
-        if (this.options.classOveride) {
-            overlay.addClass(this.options.classOveride);
+            // Target class override in order to host the Loader
+            if (this.options.classOveride) {
+                overlay.addClass(this.options.classOveride);
+            }
+
+            // Overlay placement
+            container.append(
+                overlay.append(
+                    $('<div></div>').addClass('gif_loader')
+                ).fadeIn(this.options.duration)
+            );
+
+            this.buffer = this.buffer + 1;
         }
-
-        // Overlay placement
-        container.append(
-            overlay.append(
-                $('<div></div>').addClass('gif_loader')
-            ).fadeIn(this.options.duration)
-        );
     };
+
+    /*
+     * Loader Launch, only for top fixed loader
+     */
+    this.launch = function() {
+
+        this.init(this.buffer);
+
+        this.buffer = this.buffer + 1;
+
+    }
 
 	/*
 	 * Loader remove function
 	 */
     this.remove = function(){
-        var overlay = this.container.children(".gif_overlay");
-        if (overlay.length) {
-            overlay.fadeOut(this.options.classOveride, function() {
-                overlay.remove();
-            });
-        }              
+
+        this.buffer = this.buffer - 1;
+
+        if (this.buffer <= 1) {
+            this.buffer = 0;
+            var overlay = this.container.children(".gif_overlay");
+            if (overlay.length) {
+                overlay.fadeOut(this.options.classOveride, function() {
+                    overlay.remove();
+                });
+            }
+        }
     }
 
 	/*
@@ -84,5 +109,5 @@ function gifLoader (el, options) {
         });
     }              
 
-    this.init();
+    this.init(0);
 }
