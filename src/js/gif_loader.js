b73f13080e701f7c3d81476d3e31d0d1a0d10e6c
Jean Bordat <bordat.jean@gmail.com>
2014-10-21 21:12:12 +0200

    Plugin update
    Signed-off-by: Jean Bordat <bordat.jean@gmail.com>

--- src/js/gif_loader.js
+++ src/js/gif_loader.js
@@ -1,77 +1,89 @@
 /**
-* @brief Displays a loader
-* @details This function renders a loader gif
-* 
-* @param  div which will be loaded
-* @return nothing
-* 
-* @author Jean Bordat <bordat@gmail.com>
-* Updated by Jean Bordat <jean.bordat@steria.com> the 2014-08-15
-*/
-function gifLoader (el, options) {
-    
+ * @brief Displays a loader
+ * @details This function renders a loader gif
+ *
+ * @param  div which will be loaded
+ * @return nothing
+ *
+ * @author Jean Bordat <bordat@gmail.com>
+ * Updated by Jean Bordat <jean.bordat@steria.com> the 2014-08-15
+ */
+function gifLoader(el, options) {
+
     this.container = $(el);
     // Options default definitions
     var defaults = {
-        bgColor : '#F5F5F5',
-        duration : 800,
-        opacity : 0.7,
-        classOveride : false,
-        marginTop : (1 - (this.container.height() + 1))
+        bgColor: '#F5F5F5',
+        duration: 800,
+        opacity: 0.7,
+        classOveride: false,
+        marginTop: 0,
+        top: 0
     };
     this.options = jQuery.extend(defaults, options);
-    
+
+    this.queue = 0;
+
     /**
      * Init function
      */
-    this.init = function() {
-        var container = this.container;
+    this.init = function () {
 
-        // Delete previous active loaders in this element
-        this.remove(); 
+        if (this.queue === 0) {
+            var container = this.container;
 
-        // Div "Loader" initialisation
-        var overlay = $('<div id="loader"></div>').css({
-            'background-color': this.options.bgColor,
-            'opacity':this.options.opacity,
-            'width': (container.width() + 0),
-            'height': (container.height() + 0),
-            'position':'relative',
-            'top':'0px',
-            'left':'0px',
-            'z-index':99999,
-            'margin-top': this.options.marginTop
-        }).addClass('gif_overlay');
+            // Delete previous active loaders in this element
+            this.remove(false);
 
-        // Target class override in order to host the Loader
-        if (this.options.classOveride) {
-            overlay.addClass(this.options.classOveride);
+            // Div "Loader" initialisation
+            var overlay = $('<div id="loader"></div>').css({
+                'background-color': this.options.bgColor,
+                'opacity': this.options.opacity,
+                'width': (container.width() + 0),
+                'height': (container.height() + 0),
+                'position': 'relative',
+                'top': this.options.top,
+                'left': '0px',
+                'z-index': 99999,
+                'margin-top': this.options.marginTop
+            }).addClass('gif_overlay');
+
+            // Target class override in order to host the Loader
+            if (this.options.classOveride) {
+                overlay.addClass(this.options.classOveride);
+            }
+
+            // Overlay placement
+            container.html(
+                overlay.html(
+                    $('<div></div>').addClass('gif_loader')
+                ).fadeIn(this.options.duration)
+            );
         }
-
-        // Overlay placement
-        container.append(
-            overlay.append(
-                $('<div></div>').addClass('gif_loader')
-            ).fadeIn(this.options.duration)
-        );
+        this.queue++;
     };
 
     /*
      * Loader remove function
      */
-    this.remove = function(){
+    this.remove = function (count) {
+        if (typeof count === 'undefined') {
+            this.queue--;
+        }
         var overlay = this.container.children(".gif_overlay");
-        if (overlay.length) {
-            overlay.fadeOut(this.options.classOveride, function() {
-                overlay.remove();
-            });
-        }              
+        if (this.queue === 0) {
+            if (overlay.length) {
+                overlay.fadeOut(this.options.classOveride, function () {
+                    overlay.remove();
+                });
+            }
+        }
     };
 
     /*
      * Function updating Gif placement in case of div size change
      */
-    this.update_size = function(){
+    this.update_size = function () {
         var container = this.container;
         var loader = container.find('#loader');
 
@@ -81,7 +93,7 @@ function gifLoader (el, options) {
             'height': (container.height() + 0),
             'margin-top': (1 - (container.height() + 1))
         });
-    };       
+    };
 
     this.init();
 }
